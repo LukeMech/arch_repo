ece00ac81727357be8f5b6978e7fd4f0c34684d1
LukeMech <lukaszjajkommz@gmail.com>
2024-10-07 19:15:17 +0200

    ;p

--- repo/scripts/repo.sh
+++ repo/scripts/repo.sh
@@ -2,7 +2,6 @@
 set -e
 
 files_found=$(find /artifacts -type f -name "*.pkg*" 2>/dev/null)
-should_update_huge="false"
 
 if [[ -n "$files_found" ]]; then
 	for pkg in /artifacts/**/*.pkg*; do
@@ -45,7 +44,6 @@ if [[ -n "$files_found" ]]; then
 			done
 			mv ./* ../
 			cd ../..
-			should_update_huge="true"
 			
 		else
 			# Filesize < 80mb
@@ -55,28 +53,6 @@ if [[ -n "$files_found" ]]; then
 
 	done
 	
-	if [ "$should_update_huge" == "true" ]; then
-		echo "[INFO] Rebuilding huge updater package..."
-		pkgver=$(printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short=7 HEAD)")
-		cd ../../lukemech-huge
-		mkdir -p ../tmp/build
-		cp -r ./* ../tmp/build
-		cd ../tmp
-		sed -i "s/{PKGVER}/$pkgver/g" build/PKGBUILD
-                sed -i "s/{RUNNUM}/$RUNNUM/g" build/PKGBUILD
-		bash ../repo/scripts/build.sh
-		
-		cd ../repo/x86_64/
-		echo "[INFO] Signing and adding to repo..."
-		for pkg in ../../tmp/**/*.pkg*; do
-			pkg_file=$(basename "${pkg}")
-			mv "${pkg}" .
-			gpg --use-agent --batch --yes --output ./"${pkg_file}.sig" --detach-sig ./"${pkg_file}"
-			repo-add  -v -R -s lukemech.db.tar.gz ./"${pkg_file}" &> /dev/null
-		done
-		rm -rf ../../tmp
-		
-	fi
 	
 	echo "[INFO] Removing localBuild..."
 	rm -rf ../../localBuild/*.pkg*
